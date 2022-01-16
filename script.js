7cbb58c9f5146dfe6ac45f889b03ccd892be6b4d
Raffi Maulana Hadi <raffimaulana25@gmail.com>
2022-01-17 02:27:17 +0700

    add deposits function

--- script.js
+++ script.js
@@ -116,4 +116,16 @@ const movementsDescription = movements.map(
     )}`
 );
 
-console.log(movementsDescription);
+const deposits = movements.filter(function (mov) {
+  return mov > 0;
+});
+
+console.log(movements);
+console.log(deposits);
+
+const depositsFor = [];
+for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+console.log(depositsFor);
+
+const withdrawals = movements.filter(mov => mov < 0);
+console.log(withdrawals);
